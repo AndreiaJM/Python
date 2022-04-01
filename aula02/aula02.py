21063c2f47f395c83e632824e08f6a31d7cc3ebc
AndreiaJM <andreia.ajms@gmail.com>
2022-04-01 13:20:24 -0300

    Aprendendo python aula 1 e 2

---
+++ aula02/aula02.py
@@ -0,0 +1,21 @@
+#hashtag para comentario de uma unica linha
+#Enter e  ponto e virgula são o terminadores de comando em python
+#Em python não se especifica o tipo de variavel 
+#As variáveis não são tipardas e podem mudar e numero para string
+
+"""Aspas triplas para comentario de multiplas linhas
+
+num = 1
+num = "Andreia"
+
+num1=num2=res=0 inicializando varias variáveis com mesmo valor
+
+"""
+num1=num2=res=0
+canal = "CFB Cursos";curso = "Curso de Python"
+print(canal + " " +curso)
+
+if 10>2:
+    print("Maior") #O recuo define o que fara parte daquele escopo, precisa ser 
+    print("Aula 2")#o mesmo espaço para tudo dentro daquele escopo
+print("FIM")
